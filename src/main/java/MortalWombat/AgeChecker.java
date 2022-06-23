b34b09780aab9990e4389309c27fc7cc71c21202
Dmitry Dolinsky <dmitry.dolinsky@ab-soft.net>
2022-06-23 23:12:08 +0300

    New autotest with young user

--- src/main/java/MortalWombat/AgeChecker.java
+++ src/main/java/MortalWombat/AgeChecker.java
@@ -1,2 +1,9 @@
-package MortalWombat;public class AgeChecker {
+package MortalWombat;
+
+public class AgeChecker {
+    private final int MIN_AGE = 18;
+    public boolean canPlayGame(int age) {
+        return age >= MIN_AGE;
+    }
+
 }
--- src/test/java/MortalWombat/AgeCheckerTest.java
+++ src/test/java/MortalWombat/AgeCheckerTest.java
@@ -1,2 +1,19 @@
-package MortalWombat;public class AgeCheckerTest {
+package MortalWombat;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AgeCheckerTest {
+
+    @Test
+    public void testAgedUserCanPlay() {
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertTrue(ageChecker.canPlayGame(19), "Aged user can't play game");
+    }
+
+    @Test
+    public void testThatTooYoungUsersCanNotPlay() {
+       AgeChecker ageChecker = new AgeChecker();
+       Assert.assertFalse(ageChecker.canPlayGame(12), "Young user can play game");
+    }
 }
